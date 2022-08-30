875384e12a5dd06989aa1bc15a10217b1d16a32d
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-08-30 05:38:26 -0300

    concluido o exercicio

--- Aula 3/Array - Lista Ordenada/script.js
+++ Aula 3/Array - Lista Ordenada/script.js
@@ -1,5 +1,22 @@
-document.querySelector("#botao").addEventListener("click", arrayOrganization);
+document.querySelector("#button").addEventListener("click", arrayOrganization);
+userArray = [];
 
 function arrayOrganization(){
     
+    let arrayList = document.querySelector(".arrayList");
+    while (arrayList.firstChild){
+        arrayList.removeChild(arrayList.firstChild);
+    }
+
+    userArray.push(document.querySelector("#word").value);
+    userArray.sort();
+
+    for(i of userArray){
+        e = document.createElement("li"); 
+        e.appendChild(document.createTextNode(i));
+        e.setAttribute("class", "arrayItem");
+        document.querySelector("ol").appendChild(e);
+    }
+
+    console.log(userArray)
 }
