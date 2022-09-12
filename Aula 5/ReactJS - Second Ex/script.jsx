1f971ddbbfd80e2da81a9bacaa90e7a37ecc12c8
Martarelli <renanmartarelli@gmail.com>
2022-09-12 20:33:28 -0300

    exemplo 2, uso de interpolação com função

--- Aula 5/ReactJS - Second Ex/script.jsx
+++ Aula 5/ReactJS - Second Ex/script.jsx
@@ -1,2 +1,12 @@
-const element = <h1>Hello World</h1>
+const user = {
+    firstName: "Renan",
+    lastName: "Martarelli"
+};
+
+function formatName(user){
+    return user.firstName + " " + user.lastName;
+};
+
+
+const element = <h1>Hello {formatName(user)}</h1>;
 ReactDOM.render(element, document.getElementById("root"));
