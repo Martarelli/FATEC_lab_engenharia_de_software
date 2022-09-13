6516eeb02e350badf3e8fe1d686c40d17491acd5
Martarelli <renanmartarelli@gmail.com>
2022-09-12 21:18:54 -0300

    exemplo 3 concluido

--- Aula 5/ReactJS - Third Ex/script.jsx
+++ Aula 5/ReactJS - Third Ex/script.jsx
@@ -7,6 +7,19 @@ function formatName(user){
     return user.firstName + " " + user.lastName;
 };
 
+// const element = (
+//     <h1 className="greeting">
+//         Hello World!
+//     </h1>
+// );
 
-const element = <h1>Hello {formatName(user)}</h1>;
+const element = (
+        "h1",
+        {className: "greeting"},
+        "Hello World!"
+    );
+
+
+
+
 ReactDOM.render(element, document.getElementById("root"));
