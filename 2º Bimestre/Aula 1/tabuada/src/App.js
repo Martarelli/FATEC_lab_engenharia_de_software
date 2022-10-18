5fb5abc477c0016ad2463c925fca5437f77e9417
Martarelli <renanmartarelli@gmail.com>
2022-10-17 21:51:24 -0300

    adicionado componente no app

--- 2º Bimestre/Aula 1/tabuada/src/App.js
+++ 2º Bimestre/Aula 1/tabuada/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Titulo from './Titulo';
 import Titulo2 from './Titulo2';
 import Tabuada from './Tabuada';
+import Cores from './Cores'
 
 let n = parseInt(prompt("Digite um número: "));
 
 function App() {
   return (
     <div className="App">
+      <Cores />
       <Titulo 
             titulo= "FATEC"
             texto= "Faculdade de Tecnologia de Presidente Prudente"
