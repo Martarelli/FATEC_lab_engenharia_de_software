b501fb16725eb45578e6c82a11dbf4674fde792e
Martarelli <renanmartarelli@gmail.com>
2022-10-17 21:11:12 -0300

    implementação de componentes

--- 2º Bimestre/Aula 1/tabuada/src/App.js
+++ 2º Bimestre/Aula 1/tabuada/src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
 import './App.css';
 
 let n = parseInt(prompt("Digite um número: "))
 
+class Titulo extends React.Component {
+  render(){
+    return <h2>Componente titulo com CLASSE</h2>
+  }
+}
 
+function Titulo2(){
+  return <h2>Componente Titulo com FUNÇÃO</h2>
+}
 
 function App() {
   return (
     <div className="App">
+      <Titulo/>
+      <Titulo2/>
+
       <h1>Tabuada do {n}</h1>
       <h3> 1 x {n} = {1 * n}</h3>
       <h3> 2 x {n} = {2 * n}</h3>
