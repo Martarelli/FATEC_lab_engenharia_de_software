406108928851d96eaaf73ff3921766a7cb6cb495
Martarelli <renanmartarelli@gmail.com>
2022-10-24 20:27:44 -0300

    component header funcionando

--- 2º Bimestre/Aula 1/tabuada/src/Header.js
+++ 2º Bimestre/Aula 1/tabuada/src/Header.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            color: "black",
+            ciclo: "render"                        
+        };
+    }
 
+    static getDerivedStateFromProps(){
+        this.setState({color: "orange", ciclo: "getDerivedStateFromProps"})
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({color: "blue", ciclo: "componentDidMount"})
+        }, 1000);
+    }
+
+    render(){
+        return (
+            <h1>A cor selecionada no state é: <b>{this.state.color}</b> no ciclo <b>{this.state.ciclo}</b></h1>
+        )
+    }
 }
 
 export default Header;
