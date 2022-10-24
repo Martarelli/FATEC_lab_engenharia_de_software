e686273554c086b966bd5f7c412410a61d8b4ea3
Martarelli <renanmartarelli@gmail.com>
2022-10-24 20:42:26 -0300

    finish component Container

--- 2º Bimestre/Aula 1/tabuada/src/Container.js
+++ 2º Bimestre/Aula 1/tabuada/src/Container.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 class Container extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {show: true};
+    }
 
+    deleteHeader = () => {
+        this.setState({show: false});
+    }
+
+    render(){
+        let minhaHeader;
+        if(this.state.show){
+            minhaHeader = <Child/>;
+        }
+
+        return(
+            <div>
+                {minhaHeader}
+                <button type="button" onClick={this.deleteHeader}>Desmonte o componente</button>
+            </div>
+        )
+
+    }
+
+}
+
+class Child extends React.Component{
+    componentWillUnmount(){
+        alert("O componente está sendo desmontado");
+    }
+    render(){
+        return (
+            <h1>Componente está montado</h1>
+        )
+    }
 }
 
 export default Container;
