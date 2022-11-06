385c398c6a0610d85b8af97f88c8676625b48206
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-11-06 19:15:53 -0300

    criado contador component

---
+++ 2º Bimestre/Aula 1/tabuada/src/Contador.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Contador extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {segundos:0}
+    }
+}
+
+export default Contador;
