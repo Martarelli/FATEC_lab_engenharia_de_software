ec852c9448c3e17a989ddcdb879ba3f25cddcb2c
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-11-06 19:30:07 -0300

    implementação inicial do TodoList

--- 2º Bimestre/Aula 1/tabuada/src/TodoList.js
+++ 2º Bimestre/Aula 1/tabuada/src/TodoList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {title: "Todo List"};
+    }
 
+    render(){
+        return(
+            <div>
+                <h1> {this.state.title} </h1>
+            </div>
+        )
+    }
 }
 
 export default TodoList;
