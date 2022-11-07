bda6ba9e146986e80b1dfe1104e10007de5ef449
Martarelli <renanmartarelli@gmail.com>
2022-11-07 20:35:27 -0300

    todolist finalizado

--- 2º Bimestre/Aula 1/tabuada/src/App.js
+++ 2º Bimestre/Aula 1/tabuada/src/App.js
@@ -1,34 +1,72 @@
 import React from 'react';
 import './App.css';
-import Titulo from './Titulo';
-import Titulo2 from './Titulo2';
-import Tabuada from './Tabuada';
-import Cores from './Cores'
-import Mensagem from './Mensagem'
-import Header from './Header'
-import Container from './Container'
+// import Titulo from './Titulo';
+// import Titulo2 from './Titulo2';
+// import Tabuada from './Tabuada';
+// import Cores from './Cores'
+// import Mensagem from './Mensagem'
+// import Header from './Header'
+// import Container from './Container'
 import Contador from './Contador'
 import TodoList from './TodoList';
 
-let n = parseInt(prompt("Digite um número: "));
+// let n = parseInt(prompt("Digite um número: "));
 
-function App() {
-  return (
-    <div className="App">
-      <TodoList />
-      <Contador />
-      <Container />
-      <Header />
-      <Mensagem />
-      <Cores />
-      <Titulo 
-            titulo= "FATEC"
-            texto= "Faculdade de Tecnologia de Presidente Prudente"
-            link= "www.fatec.edu.br"/>
-      <Titulo2/>
-      <Tabuada numero={n}/>  
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      itens: [],
+      text: ""
+    }
+  }
+
+  handleChange=(e)=>{
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if(!this.state.text.length){
+      return;
+    }
+
+    const item = {
+      id: Date.now(),
+      text: this.state.text
+    }
+
+    this.setState(state => ({
+      itens: state.itens.concat(item),
+      text: ''
+    }));
+
+  }
+  render(){
+    return (
+      <div className="App">
+        <TodoList itens= {this.state.itens}/>
+        <br/>
+        <form onSubmit={this.handleSubmit}>
+          <input id="new-todo" onChange={this.handleChange} value={this.state.text}/>
+          <button>Inserir</button>
+        </form>
+        <Contador />
+        {/* <Container />
+        <Header />
+        <Mensagem />
+        <Cores />
+        <Titulo 
+              titulo= "FATEC"
+              texto= "Faculdade de Tecnologia de Presidente Prudente"
+              link= "www.fatec.edu.br"/>
+        <Titulo2/>
+        <Tabuada numero={n}/>   */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- 2º Bimestre/Aula 1/tabuada/src/TodoList.js
+++ 2º Bimestre/Aula 1/tabuada/src/TodoList.js
@@ -3,13 +3,20 @@ import React from "react";
 class TodoList extends React.Component{
     constructor(props){
         super(props);
-        this.state = {title: "Todo List"};
+        this.state = {
+            title: "Todo List",
+          }
     }
 
     render(){
         return(
             <div>
                 <h1> {this.state.title} </h1>
+                <ul>
+                    {this.props.itens.map(
+                        item => (<li key={item.id}>{item.text}</li>)
+                    )}
+                </ul>
             </div>
         )
     }
