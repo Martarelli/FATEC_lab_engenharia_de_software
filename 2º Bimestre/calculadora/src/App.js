1e21e28167b9d449ebc98f50bde33b0ab38bd06f
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-11-18 18:31:23 -0300

    adicionado novas funcões

--- 2º Bimestre/calculadora/src/App.js
+++ 2º Bimestre/calculadora/src/App.js
@@ -1,43 +1,125 @@
+import React from 'react';
 import './App.css';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      n1: 0,
+      n2: 0,
+      display: 0,
+      operation: false,
+      operationType: ""
+    }
+  }
 
-function App() {
-  return (
-    <div className = 'app'>
-      <div className ='row-display'>
-        <div className='display'>0</div>
-      </div>
-      <div className ='rows'>
-        <div className='button button-light'>AC</div>
-        <div className='button button-light'>+/-</div>
-        <div className='button button-light'>%</div>
-        <div className='button button-operation'>/</div>
-      </div>
-      <div className ='rows'>
-        <div className='button'>7</div>
-        <div className='button'>8</div>
-        <div className='button'>9</div>
-        <div className='button button-operation'>X</div>
-      </div>
-      <div className ='rows'>
-        <div className='button'>4</div>
-        <div className='button'>5</div>
-        <div className='button'>6</div>
-        <div className='button button-operation'>-</div>
-      </div>
-      <div className ='rows'>
-        <div className='button'>1</div>
-        <div className='button'>2</div>
-        <div className='button'>3</div>
-        <div className='button button-operation'>+</div>
-      </div>
-      <div className ='rows'>
-        <div className='button-large'>0</div>
-        <div className='button'>,</div>
-        <div className='button button-operation'>=</div>
-      </div>
-    </div>
+  gettingData = (e) => {   
+    if (!this.state.operation) {
+      if (this.state.n1 === 0 ) {
+        this.setState({
+          n1: e.target.value,
+          display: this.state.n1
+        });
+      } else {
+        this.setState({
+          n1: this.state.n1 + e.target.value,
+          display: this.state.n1
+         });
+      } 
+    } else {
+      if (this.state.n2 === 0 ) {
+        this.setState({
+          n2: e.target.value,
+          display: this.state.n2
+        });
+      } else {
+        this.setState({
+          n2: this.state.n2 + e.target.value,
+          display: this.state.n2
+        });
+      } 
+    }
 
-  );
+  }
+
+  action = (e) => {
+    if (e.target.value === "x") {
+        this.setState({
+        operation: true,
+        operationType: e.target.value,
+        display: 0
+        });
+    } else if (e.target.value === "/") {
+      this.setState({
+        operation: true,
+        operationType: e.target.value,
+        display: 0
+        });
+    } else if (e.target.value === "-") {
+      this.setState({
+        operation: true,
+        operationType: e.target.value,
+        display: 0
+        });
+    } else if (e.target.value === "+") {
+      this.setState({
+        operation: true,
+        operationType: e.target.value,
+        display: 0
+        });
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      n1: 0,
+      n2: 0,
+      display: 0,
+      operation: false,
+      operationType: ""
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className = 'app'>
+        <div className ='row-display'>
+          <div className='display'>{this.state.n1}</div>
+        </div>
+        <div className ='rows'>
+          <button className='button button-light' onClick={this.clear}>AC</button>
+          <button className='button button-light' onClick={this.action} value={"+/-"}>+/-</button>
+          <button className='button button-light' onClick={this.gettingData} value={"%"}>%</button>
+          <button className='button button-operation' onClick={this.action} value={"/"}>/</button>
+        </div>
+        <div className ='rows'>
+          <button className='button' onClick={this.gettingData} value={7}>7</button>
+          <button className='button' onClick={this.gettingData} value={8}>8</button>
+          <button className='button' onClick={this.gettingData} value={9}>9</button>
+          <button className='button button-operation' onClick={this.action} value={"x"}>X</button>
+        </div>
+        <div className ='rows'>
+          <button className='button' onClick={this.gettingData} value={4}>4</button>
+          <button className='button' onClick={this.gettingData} value={5}>5</button>
+          <button className='button' onClick={this.gettingData} value={6}>6</button>
+          <button className='button button-operation' onClick={this.action} value={"-"}>-</button>
+        </div>
+        <div className ='rows'>
+          <button className='button' onClick={this.gettingData} value={1}>1</button>
+          <button className='button' onClick={this.gettingData} value={2}>2</button>
+          <button className='button' onClick={this.gettingData} value={3}>3</button>
+          <button className='button button-operation' onClick={this.action} value={"+"}>+</button>
+        </div>
+        <div className ='rows'>
+          <button className='button button-large'>0</button>
+          <button className='button'>,</button>
+          <button className='button button-operation'>=</button>
+        </div>
+      </div>
+  
+    );
+  }
 }
 
 export default App;
